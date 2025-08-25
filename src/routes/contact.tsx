94b6a9b0356909bfd441e4a7d9627bb3772c290b
ferhatr10 <ferhatrudvanogullari6@gmail.com>
2025-08-25 11:16:00 +0300

    fix: reset form on button click in contact page

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -143,6 +143,11 @@ function ContactPage() {
                   type="submit"
                   className="w-full bg-primary hover:bg-primary/90 text-primary-foreground"
                   disabled={isSubmitting}
+                  onClick={() => {
+                  if (form.current) {
+                    form.current.reset();
+                  }
+                  }}
                 >
                   <Send className="w-4 h-4 mr-2" />
                   {isSubmitting ? "Sending..." : "Send Message"}
